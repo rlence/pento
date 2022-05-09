0144220ebcc0717d895040a8f463511b061e967c
rlence-d <ricardo@doofinder.com>
2022-05-09 15:48:25 +0200

    prducts live

--- lib/pento_web/live/page/wrong/wrong_live.ex
+++ lib/pento_web/live/page/wrong/wrong_live.ex
@@ -2,30 +2,6 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
-  def render(assigns) do
-    ~L"""
-    <h1>Your score: <%= @score %></h1>
-      <h2>
-        <%= @message %>
-      </h2>
-      <h2>
-      <%= for n <- 1..10 do %>
-        <a href="#" phx-click="guess" phx-value-number="<%= n %>"><%= n %></a>
-      <% end %>
-    </h2>
-
-    <pre>
-      <%= @current_user.email %>
-      <%= @session_id %>
-    </pre>
-
-    <%= if @is_win do %>
-      <button phx-click="restart" > Restart the game  </button>
-    <% end %>
-    """
-
-    end
-
   def initial_state(socket, session_id) do
     number_to_win = Enum.random(1..10)
     IO.inspect number_to_win
@@ -39,12 +15,12 @@ defmodule PentoWeb.WrongLive do
   end
 
   def handle_event("guess", %{"number" => guess}=data, socket) do
+    IO.inspect guess
     {select_number, _} = Integer.parse guess
-
-     {_, message, score, is_win} = cond do
-      select_number == socket.assigns.win -> {:ok, "Your guess: #{guess}. Win. Trai again", socket.assigns.score + 1, true}
-      select_number != socket.assigns.win -> {:ok, "Your guess: #{guess}. Win. Wrong again", socket.assigns.score - 1, false}
-    end
+    {_, message, score, is_win} = cond do
+    select_number == socket.assigns.win -> {:ok, "Your guess: #{guess}. Win. Trai again", socket.assigns.score + 1, true}
+    select_number != socket.assigns.win -> {:ok, "Your guess: #{guess}. Win. Wrong again", socket.assigns.score - 1, false}
+  end
 
     { :noreply, assign(socket, message: message, score: score, is_win: is_win) }
   end
