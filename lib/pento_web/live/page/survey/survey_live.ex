919a8f2b17e712863e22255c4b080841e92cd2a8
rlence-d <ricardo@doofinder.com>
2022-05-11 19:03:29 +0200

    select start

--- lib/pento_web/live/page/survey/survey_live.ex
+++ lib/pento_web/live/page/survey/survey_live.ex
@@ -3,12 +3,22 @@ defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
   alias PentoWeb.HeroComponent
   alias PentoWeb.DemographicLive.Show
+  alias PentoWeb.RatingLive.Index
   alias Pento.Survey
+  alias Pento.Catalog
 
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(socket,
       :demographic,
       Survey.get_demographic_by_user(current_user))
+  end
+
+  def assign_products(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :products, list_products(current_user))
+  end
+
+  defp list_products(user) do
+    Catalog.list_products_with_user_rating(user)
   end
 
   def handle_demographic_created(socket, demographic) do
@@ -23,6 +33,7 @@ defmodule PentoWeb.SurveyLive do
 
   def mount(_params, _session, socket) do
     {:ok, socket
-    |> assign_demographic}
+    |> assign_demographic
+    |> assign_products }
   end
 end
